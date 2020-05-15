c09d51444fbbd34794481b24e02a3b9d29b2d626
JJ <jlii0130@student.monash.edu>
2020-05-16 02:50:11 +1000

    All the function done

--- app/api/dbconnection.js
+++ app/api/dbconnection.js
@@ -102,4 +102,68 @@ app.get("/get3/:farmstage", (req, res) => {
 });
 
 
+//get the precautionary method
+app.get("/get4/:farmstage", (req, res) => {
+    var farmstage = req.params.farmstage
+    
+
+    const connection = mysql.createConnection(databaseOptions);
+
+    connection.connect(function(err) {
+        if (err) {
+            console.error('error connecting: ' + err.stack);
+            return;
+        }
+        console.log('successfully connected as id ' + connection.threadId);
+
+        //write query
+        const query = 
+        "SELECT `Preventive Measure`, `Preventive Desc`, `Preventive Application` FROM `Preventive`" + 
+        "JOIN `harvest_preventive` ON `Preventive`.`Preventive_ID` = `harvest_preventive`.`Preventive_ID`" +
+        "JOIN `Harvest` on `Harvest`.`Harvest ID` = `harvest_preventive`.`Harvest ID`" +
+        "WHERE `Harvest`.`Harvest Stage` = '" + farmstage + "';"
+
+        connection.query(query,function (err, result) {
+            if (err){
+                res.json(err);
+            }else {
+                console.log(result);
+                res.json(result);
+            }
+        });
+    });
+});
+
+app.get("/get5/:fruit", (req, res) => {
+    var fruit = req.params.fruit
+    
+
+    const connection = mysql.createConnection(databaseOptions);
+
+    connection.connect(function(err) {
+        if (err) {
+            console.error('error connecting: ' + err.stack);
+            return;
+        }
+        console.log('successfully connected as id ' + connection.threadId);
+
+        //write query
+        const query = 
+        "SELECT `Control Measure`, `Control Desc`, `Control Application` FROM `Control`" +
+        "JOIN `Control_Fact_Table` ON `Control`.`Control ID` = `Control_Fact_Table`.`Control ID`" +
+        "JOIN `Fruit` ON `Fruit`.`Fruit_ID` = `Control_Fact_Table`.`Fruit_ID`" +
+        "WHERE `Fruit`.`Fruit_Name` = '" + fruit + "';"
+        console.log(query)
+        connection.query(query,function (err, result) {
+            if (err){
+                res.json(err);
+            }else {
+                console.log(result);
+                res.json(result);
+            }
+        });
+    });
+});
+
+
 module.exports = app;
--- public/js/farmprotect.js
+++ public/js/farmprotect.js
@@ -1,11 +1,16 @@
+//var url = 'http://ttie-env.eba-9cjewynp.ap-southeast-2.elasticbeanstalk.com'
+//local host
+var url = "http://localhost:3000"
+
+
 function btn1(){
     $("#curfarming").hide()
     $("#selectfruit").show();
     $("#farmsize").hide();
     $("#stage").hide();
     $("#faceissue").hide();
-
 }
+
 function btn2(){
     $("#curfarming").hide()
     $("#selectlocation").hide();
@@ -45,6 +50,45 @@ function btn6(){
     $("#stage").hide();
     $("#faceissue").hide();
     $("#mitresult").show();
+
+    var prostage = $( "#prostage option:selected" ).text();
+    
+   
+    console.log(this.url + '/api/get3/' + prostage)
+    //var selectfruit = document.getElementById("selectionfruit");
+    //var valfruit = selectfruit.option[selectfruit.selectedIndex].value
+ 
+    //console.log(valarea)
+ 
+ 
+    //Precautionary
+    $.ajax({
+        url: this.url + '/api/get3/' + prostage,
+        
+        success:function(data) {
+            var para = '';
+            let div = $("#method1")
+            if (data.length == 0) {
+                para = '<p>(Sorry, due to our data limitation, we could not provide any suggestion for your selection)</p>'
+            } else {
+                //console.log(data)
+                //console.log(data[0]["Preventive Measure"]);
+                para = ''
+                for (i=0; i<data.length; i++){
+                let measure = data[i]["Control Measure"]///error some may have two results
+                let desc = data[i]["Control Desc"]
+                let app = data[i]["Control Application"]
+                para += "<p>" + measure + "</p>" + "<p>" + desc + "</p>" + "<p>" + app + "</p>"
+                }
+            }
+ 
+            div.empty()  
+            div.append(para)
+            console.log(div)
+        }
+    })
+
+
 }
 
 function btn7(){
@@ -56,6 +100,45 @@ function btn7(){
     $("#mitresult").hide();
     $("#precauresult").show();
 
+
+    var prostage = $( "#prostage option:selected" ).text();
+    
+   
+    console.log(this.url + '/api/get4/' + prostage)
+    //var selectfruit = document.getElementById("selectionfruit");
+    //var valfruit = selectfruit.option[selectfruit.selectedIndex].value
+ 
+    //console.log(valarea)
+ 
+ 
+    //Precautionary
+    $.ajax({
+        url: this.url + '/api/get4/' + prostage,
+        
+        success:function(data) {
+            var para = '';
+            let div = $("#method2")
+            console.log(data)
+            if (data.length == 0) {
+                para = '<p>(Sorry, due to our data limitation, we could not provide any suggestion for your selection)</p>'
+            } else {
+                //console.log(data)
+                //console.log(data[0]["Preventive Measure"]);
+                para = ''
+                for (i=0; i<data.length; i++){
+                let measure = data[i]["Preventive Measure"]///error some may have two results
+                let desc = data[i]["Preventive Desc"]
+                let app = data[i]["Preventive Application"]
+                para += "<p>" + measure + "</p>" + "<p>" + desc + "</p>" + "<p>" + app + "</p>"
+                }
+            }
+ 
+            div.empty()  
+            div.append(para)
+            console.log(div)
+
+        }
+    })
 }
 
 //jump to What fruit are you growing in your farm? (1/2)
@@ -77,4 +160,45 @@ function btn9(){
     $("#season").hide();
     $("#fruitgrow").hide();
     $("#protectmitigate").show();   
+
+
+
+    var notcurfruit = $( "#notcurfruit option:selected" ).text();
+    
+   
+    console.log(this.url + '/api/get5/' + notcurfruit)
+    //var selectfruit = document.getElementById("selectionfruit");
+    //var valfruit = selectfruit.option[selectfruit.selectedIndex].value
+ 
+    //console.log(valarea)
+ 
+ 
+    //Precautionary
+    $.ajax({
+        url: this.url + '/api/get5/' + notcurfruit,
+        
+        success:function(data) {
+            var para = '';
+            let div = $("#method3")
+            console.log(data)
+            if (data.length == 0) {
+                para = '<p>(Sorry, due to our data limitation, we could not provide any suggestion for your selection)</p>'
+            } else {
+                //console.log(data)
+                //console.log(data[0]["Preventive Measure"]);
+                para = ''
+                for (i=0; i<data.length; i++){
+                let measure = data[i]["Control Measure"]///error some may have two results
+                let desc = data[i]["Control Desc"]
+                let app = data[i]["Control Application"]
+                para += "<p>" + measure + "</p>" + "<p>" + desc + "</p>" + "<p>" + app + "</p>"
+                }
+            }
+ 
+            div.empty()  
+            div.append(para)
+            console.log(div)
+
+        }
+    })
 }
