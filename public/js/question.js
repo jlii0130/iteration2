27d5d7055b18be3173649010f7de05f4d201e5bd
Adi Nair <anai0013@student.monash.edu>
2020-05-25 18:25:38 +1000

    jquery fix

--- public/js/question.js
+++ public/js/question.js
@@ -36,7 +36,7 @@ function btn3() {
         alert("Please select an option.");
     }
     else {
-        console.log("111")
+        console.log("")
         $("#selectlocation").hide();
         $("#farmsize").hide();
         $("#selectfruit").hide();
@@ -44,51 +44,52 @@ function btn3() {
         $("#result").show();
     }
 
-}
-
-function choose() {
-
-    //var selectarea = document.getElementById("selectionarea");
-    //var valarea = selectarea.option[selectarea.selectedIndex].value
-
-    // var valarea = $( "#selectlocation option:selected" ).text();
-    var valsize = $("#selectfarmsize option:selected").text();
-    var valfruit = $("#selectfruit option:selected").text();
-
-    console.log(this.url + '/api/get/' + valfruit + '/' + valsize)
-    //var selectfruit = document.getElementById("selectionfruit");
-    //var valfruit = selectfruit.option[selectfruit.selectedIndex].value
-
-    //console.log(valarea)
 
 
-    //Precautionary
-    $.ajax({
-        url: this.url + '/api/get/' + valfruit + '/' + valsize,
+    function choose() {
 
-        success: function (data) {
-            var para = '';
-            let div = $("#method1")
-            if (data.length == 0) {
-                para = '<p>(Sorry, due to our data limitation, we could not provide any suggestion for your selection)</p>'
-            } else {
-                //console.log(data)
-                //console.log(data[0]["Preventive Measure"]);
-                para = '<p>Hello，here is our suggestion:</p>'
-                for (i = 0; i < data.length; i++) {
-                    let measure = data[i]["Preventive Measure"]///error some may have two results
-                    let desc = data[i]["Preventive Desc"]
-                    let app = data[i]["Preventive Application"]
-                    let explain = data[i]["Preventive Explanation"]
-                    para += "<p>" + measure + "</p>" + "<p>Description</p>" + "<p>" + 
-                    desc + "</p>" + "<p>Application</p>" + "<p>" + app + "</p>" + 
-                    "<p>Explanation</p>"  + "<p>" + explain + "</p>"
+        //var selectarea = document.getElementById("selectionarea");
+        //var valarea = selectarea.option[selectarea.selectedIndex].value
+
+        // var valarea = $( "#selectlocation option:selected" ).text();
+        var valsize = $("#selectfarmsize option:selected").text();
+        var valfruit = $("#selectfruit option:selected").text();
+
+        console.log(this.url + '/api/get/' + valfruit + '/' + valsize)
+        //var selectfruit = document.getElementById("selectionfruit");
+        //var valfruit = selectfruit.option[selectfruit.selectedIndex].value
+
+        //console.log(valarea)
+
+
+        //Precautionary
+        $.ajax({
+            url: this.url + '/api/get/' + valfruit + '/' + valsize,
+
+            success: function (data) {
+                var para = '';
+                let div = $("#method1")
+                if (data.length == 0) {
+                    para = '<p>(Sorry, due to our data limitation, we could not provide any suggestion for your selection)</p>'
+                } else {
+                    //console.log(data)
+                    //console.log(data[0]["Preventive Measure"]);
+                    para = '<p>Hello，here is our suggestion:</p>'
+                    for (i = 0; i < data.length; i++) {
+                        let measure = data[i]["Preventive Measure"]///error some may have two results
+                        let desc = data[i]["Preventive Desc"]
+                        let app = data[i]["Preventive Application"]
+                        let explain = data[i]["Preventive Explanation"]
+                        para += "<p>" + measure + "</p>" + "<p>Description</p>" + "<p>" +
+                            desc + "</p>" + "<p>Application</p>" + "<p>" + app + "</p>" +
+                            "<p>Explanation</p>" + "<p>" + explain + "</p>"
+                    }
                 }
-            }
 
-            div.empty()
-            div.append(para)
-            console.log(div)
-        }
-    })
-} 
+                div.empty()
+                div.append(para)
+                console.log(div)
+            }
+        })
+    }
+}
